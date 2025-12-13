96c591411e2b8973eea5cdc6c6cd58fa485efb31
taolei <taolei@vk.com>
2025-12-13 17:00:11 +0800

    数据备份功能的扩展

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -93,7 +93,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
                   </n-icon>
                 </template>
-                此链接会自动获取当前站点地址，用于在通知中添加"查看详情"按钮。
+                此链接会自动获取当前站点地址
               </n-tooltip>
             </div>
           </template>
